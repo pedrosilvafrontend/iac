483ce9b7f6cd95480759ca5a4115d76a4133d15c
pedro.silva <pedrossilva.br@gmail.com>
2026-03-13 11:04:24 -0300

    new: test new role

--- iam.tf
+++ iam.tf
@@ -11,6 +11,34 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+  assume_role_policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Principal : {
+          Federated : aws_iam_openid_connect_provider.oidc-git.arn
+        },
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          StringLike : {
+            "token.actions.githubusercontent.com:sub" : "repo:pedrosilvafrontend/iac:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
   assume_role_policy = jsonencode({
